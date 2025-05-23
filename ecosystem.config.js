10ee0b3ef8a3cdab2a88255d93e0a520e16bd3c3
fullstack_dev <dongying9159@outlook.com>
2025-05-23 16:23:21 -0600

    refactor: update server port configuration to 5000 and increase memory limit to 2024M in ecosystem and package configuration files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,16 @@ module.exports = {
     {
       name: "haidoc-frontend",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 5050",
+      args: "start -p 5000",
       instances: "max",
       exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
-        PORT: 5050,
+        PORT: 5000,
       },
       autorestart: true,
       watch: false,
-      max_memory_restart: "1024M", // Restart if memory exceeds this
+      max_memory_restart: "2024M", // Restart if memory exceeds this
       error_file: "/var/log/pm2/nextjs-error.log",
       out_file: "/var/log/pm2/nextjs-out.log",
       log_date_format: "YYYY-MM-DD HH:mm Z",
